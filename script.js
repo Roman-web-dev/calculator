82bec3c7598b2523204c716471ff17ed7d7c20bb
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-11-21 14:58:45 -0500

    Add querryselectors. Add event for populateDisplay(). Improve logic
    operate() now assigns results properly. Populate logic to be improved

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const numbers = document.querySelectorAll(".numbers")
+const operations = document.querySelectorAll(".operations")
+const display = document.querySelector("#display")
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+        populateDisplay(e.target.textContent.trim())
+    })
+})
+
 function add (a, b){        //case 0
     return a + b
 }
@@ -14,19 +24,25 @@ function divide (a, b){     //case 3
     return a / b
 }
 
+let operateResult = 0
+
 function operate (firstNum, secondNum, operator){
     switch (operator){
         case 0:
-            display.textContent = add(firstNum, secondNum)
+            operateResult = add(firstNum, secondNum)
+            display.textContent = operateResult
             break;
         case 1:
-            display.textContent = substract(firstNum, secondNum)
+            operateResult = substract(firstNum, secondNum)
+            display.textContent = operateResult
             break;
         case 2:
-            display.textContent = multiply(firstNum, secondNum)
+            operateResult = multiply(firstNum, secondNum)
+            display.textContent = operateResult
             break;
         case 3:
-            display.textContent = divide(firstNum, secondNum)
+            operateResult = divide(firstNum, secondNum)
+            display.textContent = operateResult
             break;
     }
     input = ""
