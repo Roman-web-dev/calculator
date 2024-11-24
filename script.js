c39b99f3044928b1d5496e0f063201b9b48c25d1
Roman-web-dev <175648206+Roman-web-dev@users.noreply.github.com>
2024-11-24 12:26:42 -0500

    Add brightness change on clicking. Add multiple dots prevention

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const numbers = document.querySelectorAll(".numbers")
 const operations = document.querySelectorAll(".operations")
 const display = document.querySelector("#display div")
+const buttons = document.querySelectorAll(".numbers, .modifiers")
 
+// Changes button brightness while clicking
+buttons.forEach((button) => {
+    button.addEventListener("mousedown", (e) => {
+        e.target.style.filter = "brightness(1.2)";
+    });
+
+    button.addEventListener("mouseup", (e) => {
+        e.target.style.filter = "brightness(1)";
+    });
+
+})
+
+//Populates display
 numbers.forEach((number) => {
     number.addEventListener("click", (e) => {
+        if (input.includes(".") && e.target.textContent === ".") {  // Prevents multiple dots
+            return;
+        }
+
         populateDisplay(e.target.textContent.trim())
+    
     })
 })
 
+// Math functions
 function add (a, b){        //case 0
     return a + b
 }
@@ -24,8 +44,10 @@ function divide (a, b){     //case 3
     return a / b
 }
 
-let operateResult = 0
 
+
+// Main operational function
+let operateResult = 0
 function operate (firstNum, secondNum, operator){
     switch (operator){
         case 0:
@@ -55,6 +77,7 @@ function populateDisplay (numBtn){
         input += numBtn.toString()
     }
 
+    // Reduce display text size
     switch(input.length) {
         case 7:
             display.style.fontSize = "3rem";
